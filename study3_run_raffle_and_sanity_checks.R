3cdf678e8518ea210296428def50d999dfb0e26c
Maya Mathur <mmathur@stanford.edu>
2021-06-26 06:05:24 -0700

    start gnl'zing data prep to accommodate EITHER study 1 or 3

--- study3_run_raffle_and_sanity_checks.R
+++ study3_run_raffle_and_sanity_checks.R
@@ -1,4 +1,5 @@
 
+
 # SANITY CHECKS IN THE MIDDLE OF DATA COLLECTION --------------------------------------
 
 library(here)
@@ -23,7 +24,7 @@ w1 = fread("study3_wave1_R1_from_qualtrics_noheader.csv")
 nrow(w1)
 
 w2 = fread("study3_wave2_R1_from_qualtrics_noheader.csv")
-nrow(w2) 
+nrow(w2)
 
 code.dir = here("Code (git)")
 setwd( code.dir )
@@ -50,13 +51,13 @@ w1 %>% filter( !is.na(w1$treat) ) %>%
 # look at stratified randomization
 table(w1$targetDemographics, useNA = "ifany")
 
-# almost all highly educated, and very liberal, 
+# almost all highly educated, and very liberal,
 #  but generally older than target demographics
 table(w1$highEduc)
 table(w1$party)
 summary(w1$age)
 # we will probably have to use this simplified moderator that omits age:
-# works well because it's about 50-50 
+# works well because it's about 50-50
 w1 %>% filter(!is.na(treat)) %>%
   summarise( mean(party == "Democrat" & highEduc == 1) )
 
@@ -67,7 +68,7 @@ w1 %>% filter(!is.na(treat)) %>%
   group_by(treat) %>%
   summarise( elim = mean(madeEliminatePledge, na.rm = TRUE),
              reduce = mean(madeReducePledge, na.rm = TRUE),
-             either = mean(madeEliminatePledge | madeReducePledge, na.rm = TRUE) ) 
+             either = mean(madeEliminatePledge | madeReducePledge, na.rm = TRUE) )
 # makes sense
 
 # ~ Misc --------------------------------------
@@ -77,7 +78,7 @@ w1$problemsText
 
 # # did anyone from today get to complete the survey?
 # w1$date = dateify(w1$StartDate)
-# 
+#
 # w1 %>% filter(w1$date == "2021-05-26") %>%
 #   group_by(treat) %>%
 #   summarise( n(),
@@ -87,13 +88,13 @@ w1$problemsText
 # look at distribution of dates
 w1$date2 <- as.POSIXct(w1$date)
 
-ggplot(data = w1, aes(date2, ..count..)) + 
+ggplot(data = w1, aes(date2, ..count..)) +
   geom_histogram() +
   theme_bw() + xlab(NULL) +
   scale_x_datetime(breaks = date_breaks("1 week"),
                    #labels = date_format("%Y-%b"),
-                   limits = c( min(w1$date2), 
-                              max(w1$date2) ) )
+                   limits = c( min(w1$date2),
+                               max(w1$date2) ) )
 
 # ~ Practice linking the two waves --------------------------------------
 
@@ -111,7 +112,7 @@ w2 = w2[ !duplicated(w2$ID), ]
 # only 3 bad IDs
 ( nBadIDs = sum( !w2$ID %in% w1$ID ) )
 
-# keep all subjects in w1 who were RANDOMIZED 
+# keep all subjects in w1 who were RANDOMIZED
 #  and merge in their response data
 d = left_join( w1[ !is.na(w1$treat), ],
                w2, by = "ID" )
@@ -127,7 +128,7 @@ sum( !is.na(d$problemsBin.y) & d$problemsBin.y != "")
 expect_equal( is.na(d$treat), rep(FALSE, nrow(d)) )
 
 # summary of some important variables
-t = d %>% 
+t = d %>%
   group_by(treat) %>%
   summarise( elim = mean(madeEliminatePledge),
              reduce = mean(madeReducePledge),
@@ -141,13 +142,15 @@ as.data.frame(t)
 
 # ~ Run raffle for first wave --------------------------------------
 
+# SAVE THIS
 w1.candidates = d$email[ !d$email == "" ]
 length(w1.candidates)
 
-# give 
+# give
 set.seed(451)
 w1.winners = sample(w1.candidates, 10)
 
 setwd("~/Dropbox/Personal computer/Independent studies/2020/EatingVeg RCT/Linked to OSF (EatingVeg)/Data")
 write.csv(w1.winners, "study3_w1_raffle_winners_IDENTIFIABLE_INFO.csv")
+
 
